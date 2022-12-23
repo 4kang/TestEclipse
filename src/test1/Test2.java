2d9768c3dd31149893736661264d625e0eee769f
kono mayu <konotori08@gmail.com>
2022-12-24 00:16:23 +0900

    ThirdCommit

--- src/test1/Test2.java
+++ src/test1/Test2.java
@@ -5,13 +5,15 @@ public class Test2 {
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
 		// int型の変数
-		int number;
-		number = 3;
-		System.out.println(number + 5);
+		int number1 = 5;
+		int number2 = 3;
+		System.out.println(number1 + number2);
 		
 		//string型の変数
-		String name;
-		name = "こうのまゆ";
+		String name = "こうのまゆ";
+		System.out.println("私の名前は" + name + "です。");
+		
+		name = "かわのまゆ";
 		System.out.println("私の名前は" + name + "です。");
 	}
 
